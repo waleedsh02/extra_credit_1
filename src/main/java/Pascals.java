e1bad566c84fd3bcff14ede14150d77d81dd8de4
csc229 <waleedshahid721@gmail.com>
2024-05-03 20:08:00 -0400

    Developed a function that sorts a anagram Wrote a function that merged two arrays a and b Also defined their time and space complexities. Pascal problem solved Updated

---
+++ src/main/java/Pascals.java
@@ -0,0 +1,16 @@
+public class PascalTriangle {
+    public static void main(String[] args) {
+        long lines  = 1000000000; // making the num of rows to a billion
+        long counter = count_Non_7(rows); //  for the entries that are not divisible by 7
+        System.out.println("Entries not divisible by 7: " + count); // result is shown
+    }
+
+
+    private static long count_Non_77(long rows) {
+        long counter = 0; // Initialisation
+
+        return counter; // calcutlted count
+    }
+}
+
+
